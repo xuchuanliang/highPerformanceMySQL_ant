ef6786b2b10b6e257e1474e2e7d4652e95f34216
xuchuanliangbt <xuchuanliangbt@hikbt.com>
2019-01-18 18:00:36 +0800

    '初始化导入'

---
+++ src/main/java/com/ant/highhperformancemysql/HighHperformanceMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.ant.highhperformancemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighHperformanceMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighHperformanceMysqlApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/ant/highhperformancemysql/HighHperformanceMysqlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ant.highhperformancemysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HighHperformanceMysqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
